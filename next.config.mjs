f49a4e50cf8c45e60197f91958a078364cce352f
M9china <nqobilencaza2@gmail.com>
2024-09-18 08:13:52 +0200

    chore: install dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+    assetPrefix: isProd ? '/M9china.github.io/' : '',
+    basePath: isProd ? '/M9china.github.io' : '',
+};
 
 export default nextConfig;
